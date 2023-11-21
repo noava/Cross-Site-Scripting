7f0ccd76b51f5266292bc35096c0e285f0690d91
Niklas Vezina <niklasv@uia.no>
2023-11-22 00:10:14 +0100

    added flask app

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template, request, redirect, url_for
+from app import app
+import sqlite3
+
+
+@app.route('/')
+def index():
+    conn = sqlite3.connect('database.db')
+    data = conn.execute('SELECT * FROM user_data').fetchall()
+    conn.close()
+    return render_template('index.html', data=data)
+
+@app.route('/add_comment', methods=['POST'])
+def add_comment():
+    username = request.form['username']
+    comment = request.form['comment']
+
+    conn = sqlite3.connect('database.db')
+    conn.execute('INSERT INTO user_data (username, comment) VALUES (?, ?)', (username, comment))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for('index'))
